15d9cf6d3b6db76c8f9440a06a7a1784d20b0161
SiddharthMeyan <siddharthmeyan@gmail.com>
2021-04-06 23:58:11 +0530

    Added:Calender and int and str to urls

--- events/urls.py
+++ events/urls.py
@@ -3,5 +3,6 @@ from events import views
 
 urlpatterns = [
     path('',views.home,name='home'),
+    path('<int:year>/<str:month>/',views.home,name='home'),
     path('contact',views.contact, name='contact'),
 ]
--- events/views.py
+++ events/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
+import calendar
+from calendar import HTMLCalendar
+from datetime import datetime
+
 
 # Create your views here.
-def home(requests):
-    return render(requests,'home.html')
+def home(request,year=datetime.now().year,month=datetime.now().strftime('%B')):
+    
+    month=month.capitalize()
+    all_month=['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'] 
+    if month in all_month:                                   #IF month is in fullform eg: january , february or March
+        month_number= list(calendar.month_name).index(month) #convert month to number
+    else:                                                    #IF month is in shortform eg: jan, feb or Mar
+        month_number= list(calendar.month_abbr).index(month) #convert month to number
+    #creating a calendar on the webpage itself
+    cal = HTMLCalendar().formatmonth(year,month_number)
+    day=datetime.now().day
+    context={'month':month, 'year':year,'month_number':month_number,'cal':cal,'day':day}
+    return render(request,'home.html',context)
 
 
-def contact(requests):
-    return render(requests, 'contact.html')
+def contact(request):
+    return render(request, 'contact.html')
